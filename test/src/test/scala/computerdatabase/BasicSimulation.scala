9d408541d5999c095bbcc5f50b0f630ef5dc3690
parvas <parvas@yandex.ru>
2017-11-08 20:15:19 +0300

    gatling test module added

---
+++ test/src/test/scala/computerdatabase/BasicSimulation.scala
@@ -0,0 +1,27 @@
+package computerdatabase
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://localhost:8080") // Here is the root for all relative URLs
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+
+  val scn1 = scenario("Scenario 1") // A scenario is a chain of requests and pauses
+    .exec(http("get list")
+      .get("/list"))
+
+  val scn2 = scenario("Scenario 2") // A scenario is a chain of requests and pauses
+    .exec(http("add user")
+    .post("/add")
+    .formParam("""name""", """123""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
+    .formParam("""pass""", """123"""))
+
+  setUp(scn2.inject(atOnceUsers(1000)).protocols(httpConf))
+}
